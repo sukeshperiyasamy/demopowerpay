c1338c8de07b72e3b8a803480fa8a92503e3bd96
sukeshalex <sukeshsss200@gmail.com>
2023-05-01 23:52:27 +0530

    third commit

--- db.js
+++ db.js
@@ -20,6 +20,25 @@ mongoose.connect('mongodb://127.0.0.1:27017/mydatabase', { useNewUrlParser: true
 const User = require('./models/user'); // define user model
 app.use(express.static(__dirname));
 
+
+
+
+// Authentication middleware
+const authenticateUser = (req, res, next) => {
+  // Check if the user is logged in
+  if (req.session.user) {
+    // User is authenticated, proceed to the next middleware or route handler
+    next();
+  } else {
+    // User is not authenticated, redirect to the login page
+    res.redirect('/login');
+  }
+};
+
+
+
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -35,74 +54,82 @@ app.get('/login', (req, res) => {
 app.get('/signup', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+app.get('/forgotpassword', (req, res) => {
+  res.sendFile(__dirname + '/forgotpassword.html');
+});
 
 // ...
 
 // Signup route
 app.post('/signup', (req, res, next) => {
+  const { email, password, confirmPassword } = req.body;
+
   // Check if email is already taken
-  User.findOne({ email: req.body.email })
+  User.findOne({ email })
     .then(user => {
       if (user) {
-        return res.send(`
-          <h2>Email already exists</h2>
-          <a href="/">Go back</a>
-        `);
+        // Email already exists, redirect to login
+        return res.redirect('/login');
       }
-      // Hash the password
-      bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-          const user = new User({
-            email: req.body.email,
-            password: hash
-          });
-          // Save user to database
-          user.save()
-            .then(result => {
-              // Redirect to dashboard.html
-              res.redirect('/dashboard');
-            })
-            .catch(err => {
-              console.log(err);
-              res.send(`
-                <h2>An error occurred</h2>
-                <a href="/">Go back</a>
-              `);
-            });
+
+      // Create a new user
+      const newUser = new User({
+        email,
+        password
+      });
+
+      // Save user to database
+      newUser.save()
+        .then(result => {
+          // Redirect to dashboard
+          res.redirect('/dashboard');
+        })
+        .catch(err => {
+          console.log(err);
+          res.send(`
+            <h2>An error occurred</h2>
+            <a href="/">Go back</a>
+          `);
         });
     });
 });
-// ...
+
+
+
 
 // Login route
 app.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+
   // Check if email exists
-  User.findOne({ email: req.body.email })
+  User.findOne({ email })
     .then(user => {
       if (!user) {
-        return res.send(`
-          <h2>Authentication failed</h2>
-          <a href="/">Go back</a>
-        `);
+        // User does not exist, redirect to signup
+        return res.redirect('/signup');
       }
+
       // Compare password hashes
-      bcrypt.compare(req.body.password, user.password, (err, result) => {
-        if (err) {
-          return res.send(`
-            <h2>Authentication failed</h2>
-            <a href="/">Go back</a>
-          `);
-        }
-        if (result) {
-          // Redirect to dashboard.html
-          res.redirect('/dashboard.html');
-        } else {
+      bcrypt.compare(password, user.password)
+        .then(result => {
+          if (result) {
+            // Password matches, redirect to the dashboard
+            return res.redirect('/dashboard');
+          } else {
+            // Password does not match, redirect to login
+            return res.send(`
+              <h2>Invalid email or password</h2>
+              <a href="/login">Go back</a>
+            `);
+          }
+        })
+        .catch(err => {
+          console.log(err);
           res.send(`
-            <h2>Authentication failed</h2>
+            <h2>An error occurred</h2>
             <a href="/">Go back</a>
           `);
-        }
-      });
+        });
     })
     .catch(err => {
       console.log(err);
@@ -113,9 +140,6 @@ app.post('/login', (req, res, next) => {
     });
 });
 
-// ...
-
-// Serve static files from the "public" directory
 
 
 app.listen(4000, () => {
